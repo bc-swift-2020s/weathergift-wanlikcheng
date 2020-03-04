277a5941045c1531d902bc66be25c370d612126d
kelvin <chengwj@bc.edu>
2020-03-03 17:32:48 -0800

    Completed - Ch 7.2 WeatherLocations List Table View

---
+++ WeatherGfit/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGfit
+//
+//  Created by Kelvin Cheng on 3/3/20.
+//  Copyright © 2020 Kelvin Cheng. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
