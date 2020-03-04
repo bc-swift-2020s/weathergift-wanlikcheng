f56daf1d7d1a9ec42ade2879bd8e9188b75234be
kelvin <chengwj@bc.edu>
2020-03-03 16:51:04 -0800

    Initial Commit

---
+++ WeatherGfit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGfit
+//
+//  Created by Kelvin Cheng on 3/3/20.
+//  Copyright © 2020 Kelvin Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
