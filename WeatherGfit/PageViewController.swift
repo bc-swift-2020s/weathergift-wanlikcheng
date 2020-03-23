d04b0bafd7d9d588b87bec3b970e7e3d7cdbe178
kelvin <chengwj@bc.edu>
2020-03-22 17:30:59 -0700

    Completed - Ch. 7.8 Using the UIPageViewController

---
+++ WeatherGfit/PageViewController.swift
@@ -0,0 +1,70 @@
+//
+//  PageViewController.swift
+//  WeatherGfit
+//
+//  Created by Kelvin Cheng on 3/22/20.
+//  Copyright © 2020 Kelvin Cheng. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    
+    var weatherLocations: [WeatherLocation] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.delegate = self
+        self.dataSource = self
+        loadLocations()
+        setViewControllers([createLocationDetailViewController(forPage: 0)], direction: .forward, animated: false, completion: nil)
+    }
+    
+    func loadLocations() {
+        guard let locationsEncoded = UserDefaults.standard.value(forKey: "weatherLocations") as? Data else {
+            print("Warning: could not load weatherLocations from UserDefaults")
+            //TODO: Get user location for first element
+            weatherLocations.append(WeatherLocation(name: "Current Location", latitude: 0.0, longitude: 0.0))
+            return
+        }
+        let decoder = JSONDecoder()
+        if let weatherLocations = try? decoder.decode(Array.self, from: locationsEncoded) as [WeatherLocation] {
+            self.weatherLocations = weatherLocations
+        } else {
+            print("Error: couldn't decode data read from userDefaults")
+        }
+        if weatherLocations.isEmpty {
+            //TODO: Get user location for first element
+            weatherLocations.append(WeatherLocation(name: "Current Location", latitude: 0.0, longitude: 0.0))
+        }
+    }
+    
+    func createLocationDetailViewController(forPage page: Int) -> LocationDetailViewController{
+        let detailViewController = storyboard!.instantiateViewController(identifier: "LocationDetailViewController") as! LocationDetailViewController
+        detailViewController.locationIndex = page
+        return detailViewController
+    }
+}
+
+extension PageViewController: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? LocationDetailViewController {
+            if currentViewController.locationIndex > 0 {
+                return createLocationDetailViewController(forPage: currentViewController.locationIndex - 1)
+            }
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? LocationDetailViewController {
+            if currentViewController.locationIndex < weatherLocations.count - 1 {
+                return createLocationDetailViewController(forPage: currentViewController.locationIndex + 1)
+            }
+        }
+        return nil
+    }
+    
+    
+}
